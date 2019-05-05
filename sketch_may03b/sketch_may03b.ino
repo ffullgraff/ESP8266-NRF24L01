51b5b1822416f7cab9503064bfdb813bddedbf66
ffullgraff <ffullgraff@hotmail.com>
2019-05-05 13:45:35 -0400

    Add files via upload

---
+++ sketch_may03b/sketch_may03b.ino
@@ -0,0 +1,28 @@
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+
+char auth[] = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+
+
+char ssid[] = "your wifi";
+char pass[] = "your pasword";
+
+
+
+void setup()
+{
+ 
+  Serial.begin(9600);
+
+  Blynk.begin(auth, ssid, pass);
+  
+}
+
+void loop()
+{
+  Blynk.run();
+  
+}
